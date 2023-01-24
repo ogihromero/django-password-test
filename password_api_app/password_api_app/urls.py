b247f2bb42466fceea0c246c843f473e4e2935a0
Adriano Romero <adriano.romerofco@gmail.com>
2023-01-24 15:40:49 -0300

    feat(api): implementação verificação de password

--- password_api_app/password_api_app/urls.py
+++ password_api_app/password_api_app/urls.py
@@ -15,7 +15,67 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+from ninja import Schema
+
+
+class Rule(Schema):
+    rule: str
+    value: str
+
+
+class Item(Schema):
+    password: str
+    rules: list[Rule]
+
+
+class processedRules(Schema):
+    verify: bool
+    noMatch: list[str]
+
+
+api = NinjaAPI()
+
+
+def process_rules(password, rules):
+    noMatch = []
+    verify = True
+    for rule in rules:
+        if rule.rule == "minSize":
+            if len(password) < int(rule.value):
+                noMatch.append(rule.rule)
+                verify = False
+        if rule.rule == "minUppercase":
+            if sum(1 for char in password if char.isupper()) < int(rule.value):
+                noMatch.append(rule.rule)
+                verify = False
+        if rule.rule == "minLowercase":
+            if sum(1 for char in password if char.islower()) < int(rule.value):
+                noMatch.append(rule.rule)
+                verify = False
+        if rule.rule == "minDigit":
+            if sum(1 for char in password if char.isdigit()) < int(rule.value):
+                noMatch.append(rule.rule)
+                verify = False
+        if rule.rule == "minSpecialChars":
+            if sum(1 for char in password if "!@#$%^&*()-+\/{}[]".find(char)) < int(rule.value):
+                noMatch.append(rule.rule)
+                verify = False
+        if rule.rule == "noRepeted":
+            for char in range(1, len(password)):
+                if password[char] == password[char-1]:
+                    noMatch.append(rule.rule)
+                    verify = False
+                    break
+    return processedRules(verify=verify, noMatch=noMatch)
+
+
+@api.get("/")
+def add(request, item: Item):
+    return process_rules(item.password, item.rules)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("verify/", api.urls),
 ]
